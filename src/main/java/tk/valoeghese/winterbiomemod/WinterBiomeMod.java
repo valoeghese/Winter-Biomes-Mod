19dd66a736811a678068782cee52979485692132
valoeghese <valoeghesemods@gmail.com>
2019-11-28 19:32:44 +1300

    initial commit

---
+++ src/main/java/tk/valoeghese/winterbiomemod/WinterBiomeMod.java
@@ -0,0 +1,17 @@
+package tk.valoeghese.winterbiomemod;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WinterBiomeMod implements ModInitializer {
+	
+	public static final Logger modLog = LogManager.getLogger("WinterBiomeMod");
+	
+	@Override
+	public void onInitialize() {
+		
+	}
+
+}
